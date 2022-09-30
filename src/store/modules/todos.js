cbc9598958d3bf0001d3a026741c6038826b8e55
Ugesh ROOPCHAND <uroopchand@Prodigious.mu>
2022-09-30 13:54:59 +0400

    Pass State value before load using Created life cycle for vue

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,15 +1,15 @@
-// import axios  from "axios";
+import axios  from "axios";
 
 const state = {
   todos: [
-    {
-        id: 1,
-        title: 'Todo One'
-    },
-    {
-        id: 2,
-        title: 'Todo Two'
-    }
+    // {
+    //     id: 1,
+    //     title: 'Todo One'
+    // },
+    // {
+    //     id: 2,
+    //     title: 'Todo Two'
+    // }
   ]
 
 };
@@ -18,7 +18,15 @@ const getters = {
     allTodos: (state) => state.todos
 
 };
-const actions = {};
+const actions = {
+  //we do not call mutation directly we call it commit
+  async fetchTodos(){
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+    console.log(response.data)
+  }
+
+
+};
 const mutations = {};
 export default {
     state,
