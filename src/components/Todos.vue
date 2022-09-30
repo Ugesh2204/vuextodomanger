063a8f5a26c74786bdee197877a829f797dc8c84
Ugesh ROOPCHAND <uroopchand@Prodigious.mu>
2022-09-30 13:14:27 +0400

    mapGetters

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,13 @@
 <template>
-    <h3> Todos</h3>
+    <h3>Todos</h3>
+    <div v-for="todo in allTodos" class="todo" :key="todo.id">
+      {{todo.title}}
+    </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';    
 export default {
-   name: "Todos"
+   name: "Todos",
+   computed:mapGetters(['allTodos'])
 }
 </script>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,7 +1,23 @@
 // import axios  from "axios";
 
-const state = {};
-const getters = {};
+const state = {
+  todos: [
+    {
+        id: 1,
+        title: 'Todo One'
+    },
+    {
+        id: 2,
+        title: 'Todo Two'
+    }
+  ]
+
+};
+const getters = {
+    //To display our state we need to add this in the getter
+    allTodos: (state) => state.todos
+
+};
 const actions = {};
 const mutations = {};
 export default {
